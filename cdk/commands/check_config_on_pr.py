0960724016dbf3fa9beaa5d63e0caf246d66a225
Idan Gabdank <gabdank@users.noreply.github.com>
2026-05-26 12:10:58 -0700

    Adjust stack TTL: demo 72h, staging/sandbox 24h (#29)

--- cdk/commands/check_config_on_pr.py
+++ cdk/commands/check_config_on_pr.py
@@ -2,9 +2,17 @@ def check_demo_config_on_pr():
     from infrastructure.config import config
     assert 'backend_url' not in config['environment']['demo']
     assert ('time-to-live-hours',
-            '60') in config['environment']['demo']['tags']
+            '72') in config['environment']['demo']['tags']
     assert ('turn-off-on-friday-night',
             'yes') in config['environment']['demo']['tags']
+    assert ('time-to-live-hours',
+            '24') in config['environment']['staging']['tags']
+    assert ('time-to-live-hours',
+            '24') in config['environment']['sandbox']['tags']
+    assert ('time-to-live-hours',
+            '24') not in config['environment']['dev']['tags']
+    assert ('time-to-live-hours',
+            '24') not in config['environment']['production']['tags']
 
 
 if __name__ == '__main__':
